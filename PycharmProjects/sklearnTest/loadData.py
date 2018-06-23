2819af2642bdad87720ae1b213d374c0815674a7
XiaoGongWei <xiaogongwei10@gmail.com>
2018-06-23 15:18:09 +0800

    add shell code

--- PycharmProjects/sklearnTest/loadData.py
+++ PycharmProjects/sklearnTest/loadData.py
@@ -1,5 +1,6 @@
 # import pickle
-import cPickle as pickle
+# import cPickle as pickle
+import pickle
 
 #load data
 with open("iris_data.pickle", "rb") as f:
@@ -9,8 +10,8 @@ with open("iris_data.pickle", "rb") as f:
 with open("model.pickle", "rb") as f:
     model_bak = pickle.load(f)
 
-print "mode_bak: ", model_bak
+print("mode_bak: ", model_bak)
 # print "mode predict: ", model_bak.predict(X_data)
 # print "mode True: ", Y_data
-print "pridict score: ", model_bak.score(X_data, Y_data)
+print("pridict score: ", model_bak.score(X_data, Y_data))
 
