23add07db0ea7586d490885674af81773e98bb7c
xiaogongwei <xiaogongwei10@gmail.com>
2018-08-01 18:42:03 +0800

    add matlab clip image

---
+++ Matlab_Code/clipePicture/clipePicture.m
@@ -0,0 +1,60 @@
+
+clear
+clc
+close all;
+% set init data
+window_size = [512, 512];
+save_image_num = 5;
+folder_name = 'cut_images';
+% picture_name_R = '/home/david/Documents/MyResearch/MyPaper/lidaan/OSSP/xiaogongwei/testPy/R_B.tif';
+% picture_name_G = '/home/david/Documents/MyResearch/MyPaper/lidaan/OSSP/xiaogongwei/testPy/G_B.tif';
+% picture_name_B = '/home/david/Documents/MyResearch/MyPaper/lidaan/OSSP/xiaogongwei/testPy/B_B.tif';
+
+% % read all image_name to image_data
+% image_name = {picture_name_R, picture_name_G, picture_name_B};
+% for i = 1 : numel(image_name)
+%     fprintf('Will read image: %s\n', image_name{i})
+%     image_data(:,:,i) = imread(image_name{i}); % use imread read image
+% end
+% fprintf('Sucess to you, all image have read.\n')
+image_data = imread('ice.jpg');
+% rand window to cut image
+[rows, cols] = size(image_data(:,:,1));
+rows_max = rows - window_size(1) - 1;
+cols_max = cols - window_size(2) - 1;
+row_rand_array = floor(rand(1,save_image_num)*rows_max + 1);
+col_rand_array = floor(rand(1,save_image_num)*rows_max + 1);
+% make dir 
+if ~exist(folder_name,'dir')
+   mkdir(folder_name);
+end
+% cut images
+for n = 1 : save_image_num
+    image_file_name = ['./' folder_name '/image_' num2str(n) '.tif']
+    row_begin = row_rand_array(n);
+    col_begin = col_rand_array(n);
+    row_index = row_begin : row_begin+window_size(1) - 1;
+    col_index = col_begin : col_begin+window_size(2) - 1;
+    cut_temp_image = image_data(row_index, col_index, :);
+    imwrite(cut_temp_image, image_file_name);
+    manualCreatMask( cut_temp_image,  image_file_name);
+    pause(5)
+%     choice = questdlg('I do not want work!!! ', 'Warning', 'Yes', 'No', 'No');
+%     switch choice
+%     case 'Yes'
+%         break;
+%     end
+
+end
+fprintf('Sucess to you, all image have cuted.\n')
+
+
+
+
+
+
+
+
+
+
+
---
+++ Matlab_Code/clipePicture/manualCreatMask.m
@@ -0,0 +1,73 @@
+function manualCreatMask( image_data,  image_file_name, isdelete)
+isdelete = 0;
+if nargin > 2
+    isdelete = 1;
+end
+[image_floder, file_name] = fileparts(image_file_name); % get file path and name
+% split_char = filesep;
+h_image_data = figure('Name', image_file_name, 'position', [0, 0, 500, 500]);
+hold on
+set(h_image_data, 'color', [0, 1, 1]);
+imshow(image_data, 'InitialMagnification', 'fit');
+all_position = [];
+[image_rows, image_cols, image_chanle] = size(image_data);
+all_maskImage = double(zeros(image_rows, image_cols));
+% use get mulitply Poly
+while 1
+    isWant = qusestionDlg('Do you want get Water Area');
+    if isWant == 0
+        break;
+    end
+    h_Poly = impoly(gca);
+    if isempty(h_Poly) || ~ishandle(h_image_data)
+        return;
+    end
+    new_mask = h_Poly.createMask();
+    all_maskImage = all_maskImage + double(new_mask);
+    position = getPosition(h_Poly);
+    all_position = [all_position;position];
+end
+maskImage = logical(all_maskImage);
+isFineshed = 1;
+% isFineshed = qusestionDlg('Do you get Water Area');
+if isFineshed == 1 && size(all_position, 1) > 2 
+    h_contrast = figure('Name', 'contrast');
+    subplot(1,2,1)
+    imshow(image_data, 'InitialMagnification', 'fit');
+    subplot(1,2,2)
+%     maskedImage = image_data(:,:,1) .* cast(~maskImage, 'like', image_data);
+    imshow(maskImage, 'InitialMagnification', 'fit');
+    isRight = qusestionDlg('Dou you wang save maskImage');
+    save_mask_name = [file_name '_mask.tif'];
+    path_name = fullfile(image_floder, save_mask_name);
+    if isRight == 1
+        imwrite(maskImage, path_name);
+    end
+    if ishandle(h_contrast)
+        close(h_contrast);
+    end
+else
+    if exist(image_file_name, 'file') && isdelete
+        delete(image_file_name);
+    end
+end
+
+if ishandle(h_image_data)
+    close(h_image_data);
+end
+
+end
+
+function isFineshed = qusestionDlg(infor_str)
+if nargin < 1
+   infor_str = 'Warning';
+end
+isFineshed = 0;
+choice = questdlg(infor_str, 'Warning', 'Yes', 'No', 'No');
+switch choice
+    case 'Yes'
+        isFineshed = 1;
+    case 'No'
+        isFineshed = 0;
+end
+end
