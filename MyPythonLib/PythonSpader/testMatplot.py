85d277dbca4b8cc84f268d0117b0db845a76f834
xiaogongwei <xiaogongwei10@gmail.com>
2019-01-21 14:51:11 +0800

    add store data

---
+++ MyPythonLib/PythonSpader/testMatplot.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import matplotlib
+
+
+x = range(1, 100, 1)
+y = range(1 ,100, 1)
+
+plt.plot(x, y)
+plt.show()
+plt.savefig("xiao.jpg")
---
+++ MyShellCode/updateUbuntu.sh
@@ -0,0 +1,4 @@
+sudo apt update 
+sudo apt upgrade
+sudo apt-get update
+sudo apt-get upgrade
